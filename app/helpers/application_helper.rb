979a22858ddf6e9083292f3bc7cb1b9f81cbf6a5
ken <kirie115@gmail.com>
2018-08-29 14:25:04 +0900

    up_to_4.2.4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def full_title(page_title='') #引数に代入したものをpage_titleという変数に代入
+    if page_title.empty?
+      base_title
+    else
+      page_title+ "|" + base_title
+    end
+  end
 end
