ea7488c54801562c73e4d69a3376e2ed2b268cd0
ken <kirie115@gmail.com>
2018-09-02 14:10:06 +0900

    Finished up to 5.1.2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
   def full_title(page_title='') #引数に代入したものをpage_titleという変数に代入
+    base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
       base_title
     else
-      page_title+ "|" + base_title
+      page_title + "|" + base_title
     end
   end
 end
