faed387ef28e91672be9c1cdc2c973cc5f1ba34d
ken <kirie115@gmail.com>
2018-09-11 16:38:07 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,69 @@
 class UsersController < ApplicationController
-  # GET /users/:id
+  before_action :logged_in_user, only:[:edit,:update]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+
   def show
     @user = User.find(params[:id])
-    # => app/views/users/show.html.erb
-    # debugger
   end
 
   def new
     @user = User.new
   end
 
-  # POST /users
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def create
     @user = User.new(user_params)
-    if @user.save # => Validation
+    if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-      # GET "/users/#{@user.id}" => show
     else
-      # Failure
       render 'new'
     end
   end
 
-  def user_params
-    params.require(:user).permit(
-      :name, :email, :password,
-      :password_confirmation)
+  def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合を扱う。
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
